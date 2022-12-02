dcef8bd9a3e84373b6f97d3e746d0dfee1bc22d9
Ali Amini <a.amini78@gmail.com>
2022-12-02 21:10:02 +0330

    test

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/post', function () {
+
+    $posts = \App\Models\Post::query()->get();
+
+    return response()->json($posts);
+});
 
 require __DIR__.'/auth.php';
