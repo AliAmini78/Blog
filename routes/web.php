48e8000cd70a1f9e05df0644c54631a415bbbbe3
Ali Amini <a.amini78@gmail.com>
2022-12-02 21:13:00 +0330

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    Artisan::call('migrate');
     return view('welcome');
 });
-Route::get('/post', function () {
+
+Route::get('post', function () {
 
     $posts = \App\Models\Post::query()->get();
     Artisan::call('migrate');
